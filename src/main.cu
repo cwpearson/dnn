a4936272e0e927b73b21fbc1aa7078bf7ba88aae
Carl Pearson <carl.w.pearson@gmail.com>
2017-11-01 11:25:15 -0500

    work on minibatching

--- src/main.cu
+++ src/main.cu
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <cuda_runtime.h>
 #include <math_constants.h>
+#include <vector>
 
 #include "cuda_runtime_check.hpp"
 #include "blas/saxpy.hu"
@@ -12,12 +13,20 @@
 
 const float rate = 0.2;
 const int numEpochs = 10000;
-const int trainSize = 60000;
-const int testSize = 10000;
+const int trainSize = 6000;
+const int testSize = 1000;
 const int numSMs = 120;
-const int maxBatchSize = 10;
+const int maxBatchSize = 4;
 const bool useTrainForTest = false;
 const int cudaDevice = 0;
+
+
+template <typename T> 
+T *cudaMalloc1D(const size_t d1) {
+  T *p;
+  CUDA_RUNTIME_CHECK(cudaMalloc(&p, sizeof(T) * d1));
+  return p;
+}
 
 void *cudaMalloc3D(const size_t elemSize, const size_t d1, const size_t d2, const size_t d3) {
   void *p;
@@ -44,6 +53,31 @@ __device__ void block_output_backward(float *errorOut, const float *networkOut,
     }
   }
 }
+
+
+__device__ void output_backward_batch_block(float *errorOut, const float *networkOut,
+  const int *expected, const size_t size, const int batchSize) {
+
+  #define EO(b, o) (errorOut[b * size + o])
+  #define NO(b, o) (networkOut[b * size + o])
+  #define E(b) (expected[b])
+
+  for (int b = 0; b < batchSize; ++b) {
+    // log/liklihood and softmax error
+    for (int o = threadIdx.x; o < size; o += blockDim.x) {
+      if (o == E(b)) {
+        EO(b, o) = NO(b, 0) - 1;
+      } else {
+        EO(b, o) = NO(b, 0);
+      }
+    }
+  }
+
+  #undef E
+  #undef NO
+  #undef EO
+}
+
 
 __device__ void network_forward_block(float *fc1_y, float *r1_y, float *fc2_y,
                                       float *r2_y, float *fc3_y, float *s1_y,
@@ -178,6 +212,70 @@ train_kernel(float *fc1_dw, float *fc1_db, float *fc1_dy, float *r1_dy,
   }
 }
 
+
+__global__ void
+train_batch_kernel(float *fc1_dw, float *fc1_db, float *fc1_dy, float *r1_dy,
+             float *fc2_dw, float *fc2_db, float *fc2_dy, float *r2_dy,
+             float *fc3_dw, float *fc3_db, float *fc3_dy, float *s1_dy,
+             float *fc1_y, float *r1_y, float *fc2_y, float *r2_y, float *fc3_y,
+             float *s1_y, const float *fc1_w, const float *fc1_b,
+             const float *fc2_w, const float *fc2_b, const float *fc3_w,
+             const float *fc3_b,
+             const float * const*perLearnerImg, const int * const*perLearnerLabel, const int *perLearnerBatchSize) {
+
+  // each threadBlock is a trainer
+  const int trainerIdx = blockIdx.x;
+
+  // each trainer (threadblock) has its own work space
+  // layer outputs (and inputs)
+  fc1_y = &fc1_y[trainerIdx * 1200];
+  r1_y = &r1_y[trainerIdx * 1200];
+  fc2_y = &fc2_y[trainerIdx * 100];
+  r2_y = &r2_y[trainerIdx * 100];
+  fc3_y = &fc3_y[trainerIdx * 10];
+  s1_y = &s1_y[trainerIdx * 10];
+
+  // parameter updates, error propogation
+  fc1_dw = &fc1_dw[trainerIdx * 1200 * 784];
+  fc1_db = &fc1_db[trainerIdx * 1200];
+  fc1_dy = &fc1_dy[trainerIdx * 1200];
+  r1_dy = &r1_dy[trainerIdx * 1200];
+
+  fc2_dw = &fc2_dw[trainerIdx * 100 * 1200];
+  fc2_db = &fc2_db[trainerIdx * 100];
+  fc2_dy = &fc2_dy[trainerIdx * 100];
+  r2_dy = &r2_dy[trainerIdx * 100];
+
+  fc3_dw = &fc3_dw[trainerIdx * 10 * 100];
+  fc3_db = &fc3_db[trainerIdx * 10];
+  fc3_dy = &fc3_dy[trainerIdx * 10];
+  s1_dy = &s1_dy[trainerIdx * 10];
+
+  const float *img = perLearnerImg[trainerIdx]; // batch image data
+  const int *label = perLearnerLabel[trainerIdx]; // batch label data
+  const int batchSize = perLearnerBatchSize[trainerIdx]; // size of batch
+
+  // Have SM collaboratively compute outputs for forward pass
+  network_forward_batch_block(fc1_y, r1_y, fc2_y, r2_y, fc3_y, s1_y, fc1_w, fc1_b,
+                              fc2_w, fc2_b, fc3_w, fc3_b, img, batchSize);
+
+    // compute error of final layer
+    output_backward_batch_block(s1_dy, s1_y, label, 10, batchSize);
+    softmax_backward_batch_block(fc3_dy, s1_dy, s1_y, 10, batchSize);
+
+    // backprop error
+    fc_backward_batch_block(r2_dy, fc3_dw, fc3_db, fc3_dy, r2_y, fc3_w, fc3_b, 10,
+                      100, batchSize);
+    sigmoid_backward_batch_block(fc2_dy, r2_dy, r2_y, 100, batchSize);
+    fc_backward_batch_block(r1_dy, fc2_dw, fc2_db, fc2_dy, r1_y, fc2_w, fc2_b, 100,
+                      1200, batchSize);
+    sigmoid_backward_batch_block(fc1_dy, r1_dy, r1_y, 1200, batchSize);
+    fc_backward_batch_block(0 /* dont need error at input layer */, fc1_dw, fc1_db,
+                      fc1_dy, img, fc1_w, fc1_b, 1200, 784, batchSize);
+}
+
+
+
 void init_weights(float *x, const int size, const int numInputs) {
   for (int i = 0; i < size; ++i) {
     x[i] = ((float(rand()) / RAND_MAX) * 2 - 1) * sqrt(2.0f / numInputs);
@@ -254,6 +352,15 @@ int main(void) {
   CUDA_RUNTIME_CHECK(cudaMemcpy(imgTest_d, mnistTestData,
                                 784 * mnistNumTestImages * sizeof(float),
                                 cudaMemcpyHostToDevice));
+
+  // a pointer to each learner's image data and labels for each batch
+  std::vector<float *> perLearnerImg_h(numSMs);
+  std::vector<int *> perLearnerLabel_h(numSMs);
+  std::vector<int> perLearnerBatchSize_h(numSMs);
+  float **perLearnerImg_d = cudaMalloc1D<float*>(numSMs);
+  int **perLearnerLabel_d = cudaMalloc1D<int*>(numSMs);
+  int *perLearnerBatchSize_d = cudaMalloc1D<int>(numSMs);
+
 
   // Initialize layer weights and biases
   float *fc1_w_h = new float[784 * 1200]; // layer 1 weights
@@ -351,17 +458,16 @@ int main(void) {
       const int batchSize = batchEnd - batchStart;
 
       const float *imgTestBatch_d = &imgTest_d[batchStart * 784];
-      printf("batchStart = %d\n", batchStart);
 
       network_forward_batch_kernel<<<1, blockDim>>>(
         fc1_y_d, r1_y_d, fc2_y_d, r2_y_d, fc3_y_d, s1_y_d, fc1_w_d, fc1_b_d,
         fc2_w_d, fc2_b_d, fc3_w_d, fc3_b_d, imgTestBatch_d, batchSize);
 
       // copy network output back to host
-      CUDA_RUNTIME_CHECK(cudaMemcpy(s1_y_h, s1_y_d, maxBatchSize * 10 * sizeof(float), cudaMemcpyDeviceToHost));
+      CUDA_RUNTIME_CHECK(cudaMemcpy(s1_y_h, s1_y_d, batchSize * 10 * sizeof(float), cudaMemcpyDeviceToHost));
       for (int i = 0; i < batchSize; ++i) {
         int actual = argmax(&s1_y_h[i * 10], 10);
-        const int expected = mnistTestLabels[i];
+        const int expected = mnistTestLabels[batchStart + i];
         error += -1.0f * log(s1_y_h[expected]);
         if (actual != expected) {
           ++numWrong;
@@ -369,32 +475,32 @@ int main(void) {
       }
     }
 
-    printf("%d %f %f\n", epoch, error / mnistNumTestImages,
-           float(numWrong) / mnistNumTestImages);
+    printf("epoch=%d error=%f accuracy=%f (%d wrong / %d total)\n", epoch, error / mnistNumTestImages,
+           float(numWrong) / mnistNumTestImages, numWrong, mnistNumTestImages);
 
-    for (int batchStart = 0; batchStart < mnistNumTrainImages; batchStart += maxBatchSize) {
-      const int batchEnd = min(mnistNumTrainImages, batchStart + maxBatchSize);
-      const int batchSize = batchEnd - batchStart;
+    // Do training
+    for (int imgIdx = 0; imgIdx < mnistNumTrainImages; ) {
+      // Determine where each learner's batch begins
+      for (int learnerIdx = 0; learnerIdx < numSMs; ++learnerIdx) {
+        perLearnerImg_h[learnerIdx] = &imgTrain_d[imgIdx * 784];
+        perLearnerLabel_h[learnerIdx] = &labelTrain_d[imgIdx];
+        int imgsLeft = mnistNumTrainImages - imgIdx;
+        const int batchSize = max(0 , min(maxBatchSize, imgsLeft));
+        perLearnerBatchSize_h[learnerIdx] = batchSize;
+        imgIdx += batchSize;
+        assert(imgIdx < mnistNumTrainImages);
+      }
 
-      const float *imgTrainBatch_d = &imgTrain_d[batchStart * 784];
-      printf("batchStart = %d\n", batchStart);
+      // copy that data to GPU
+      CUDA_RUNTIME_CHECK(cudaMemcpy(perLearnerImg_d, &perLearnerImg_h[0], sizeof(float *) * numSMs, cudaMemcpyHostToDevice));
+      CUDA_RUNTIME_CHECK(cudaMemcpy(perLearnerLabel_d, &perLearnerLabel_h[0], sizeof(int *) * numSMs, cudaMemcpyHostToDevice));
+      CUDA_RUNTIME_CHECK(cudaMemcpy(perLearnerBatchSize_d, &perLearnerBatchSize_h[0], sizeof(int) * numSMs, cudaMemcpyHostToDevice));
 
-      // zero out gradient updates before training
-      zero_grid<<<numSMs, blockDim>>>(fc1_dw_d, numSMs * 1200 * 784);
-      zero_grid<<<numSMs, blockDim>>>(fc1_db_d, numSMs * 1200);
-      zero_grid<<<numSMs, blockDim>>>(fc2_dw_d, numSMs * 100 * 1200);
-      zero_grid<<<numSMs, blockDim>>>(fc2_db_d, numSMs * 100);
-      zero_grid<<<numSMs, blockDim>>>(fc3_dw_d, numSMs * 10 * 100);
-      zero_grid<<<numSMs, blockDim>>>(fc3_db_d, numSMs * 10);
-      CUDA_RUNTIME_CHECK(cudaDeviceSynchronize());
-    }
-
-    train_kernel<<<gridDim, blockDim>>>(
+    train_batch_kernel<<<gridDim, blockDim>>>(
         fc1_dw_d, fc1_db_d, fc1_dy_d, r1_dy_d, fc2_dw_d, fc2_db_d, fc2_dy_d,
         r2_dy_d, fc3_dw_d, fc3_db_d, fc3_dy_d, s1_dy_d, fc1_y_d, r1_y_d,
         fc2_y_d, r2_y_d, fc3_y_d, s1_y_d, fc1_w_d, fc1_b_d, fc2_w_d, fc2_b_d,
-        fc3_w_d, fc3_b_d, imgTrain_d, labelTrain_d, mnistNumTrainImages);
-
+        fc3_w_d, fc3_b_d, perLearnerImg_d, perLearnerLabel_d, perLearnerBatchSize_d);
     CUDA_RUNTIME_CHECK(cudaDeviceSynchronize());
 
     // Apply weight updates
@@ -408,6 +514,7 @@ int main(void) {
       saxpy_grid<<<1, 32>>>(fc3_b_d, &fc3_db_d[i * 10], alpha, 10);
     }
   }
+}
 
   return 0;
 }
